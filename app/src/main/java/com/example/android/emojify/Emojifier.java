339ce6943a031c43f15d76c0a91b46fc648a6925
ksdc-aaronp <aaron.pillay@ksdc.co.za>
2020-05-07 21:31:30 +0200

    Ex1 Emojieeez

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,32 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class Emojifier {
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        if (faces.size() == 0) {
+            Toast.makeText(context, "No faces detected.", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.i("Faces", "Faces detected = " + faces.size());
+        }
+
+    }
+
+}
